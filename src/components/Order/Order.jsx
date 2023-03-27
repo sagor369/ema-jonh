d442fedd058375f3965af998f4c1dfdc9e6bc5bd
Sagor <sagor.heaking@gmail.com>
2023-03-28 00:51:23 +0600

    cart items added complete

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -4,9 +4,12 @@ import './Order.css'
 const Order = ({cart}) => {
     let total = 0
     let shippingCharge = 0
+    let quantity = 0
     for(const product of cart){
-        total = total + product.price
+        total = total + product.price *product.quantity
         shippingCharge = shippingCharge + product.shipping
+        quantity = quantity + product.quantity  
+
     }
     const tex = (total *7/100)
     const grandTotal = total + shippingCharge + tex
@@ -14,7 +17,7 @@ const Order = ({cart}) => {
   return (
     <div className="cart">
       <h2>Order Summary</h2>
-      <p>Selected Items : {cart.length}</p>
+      <p>Selected Items : {quantity}</p>
       <p>Price: ${total}</p>
       <p>Total Shipping Charge: ${shippingCharge}</p>
       <p>Tex: ${tex.toFixed(2)}</p>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,6 +4,7 @@ import Order from '../Order/Order';
 import Product from '../Product/Product';
 import './Products.css'
 
+// products load function 
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(()=>{
@@ -11,17 +12,45 @@ const Products = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     },[])
+
+    // cart items added function 
     const [cart, setCart] = useState([])
 
     const addToCart= (product)=>{
-        const newCart = [...cart,product];
+        // const newCart = [...cart,product];
+        // setCart(newCart)
+        let newCart =[]
+        const exit = cart.find(pd => pd.id === product.id)
+        if(!exit){
+             product.quantity = 1
+             newCart = [...cart,product];
+        }
+        else{
+            exit.quantity = exit.quantity +1
+            const remaining = cart.filter(pd => pd.id !== product.id)
+            newCart = [...remaining ,exit]
+        }
         setCart(newCart)
         addToDb(product.id)
     }
+
+    // local storage to cart items load 
     useEffect(()=>{
         const getCart =  getShoppingCart()
-        console.log(getCart)
-    },[])
+        const addProduct = []
+        for (const id in getCart){
+            const saveProduct = products.find(product => product.id === id )
+            if(saveProduct){
+                const quantity = getCart[id]
+                saveProduct.quantity = quantity
+                addProduct.push(saveProduct)
+            }
+            
+            setCart(addProduct)
+        }
+    },[products])
+
+
     return (
         <div className='products'>
             <div>
